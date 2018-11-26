6f133cd44e7e906ca1947e21e6e019d8b4f55cdc
mrkiiro <kiiro.crafter@gmail.com>
2018-11-26 10:56:07 +0100

    Initial commit

---
+++ SessionManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App8
+{
+    class SessionManager
+    {
+        private SessionManager _myManager;
+        public User loggedInUser;
+
+        private SessionManager()
+        {
+            loggedInUser = new User("kasper", "pass");
+        }
+
+        public SessionManager GetManager()
+        {
+            if(_myManager == null)
+            {
+                _myManager = this;
+            }
+
+            return _myManager;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App8
+{
+    class User
+    {
+        private string _userName;
+        private string _password;
+        private int _acessLevel;
+
+        public User(string username, string password)
+        {
+            _userName = username;
+            _password = password;
+            _acessLevel = AcessLevels.Anon;
+        }
+
+        public static class AcessLevels
+        {
+            public static int Customer = 1;
+            public static int Admin = 2;
+            public static int Clerk = 3;
+            public static int Anon = 4;
+        }
+
+    }
+}
