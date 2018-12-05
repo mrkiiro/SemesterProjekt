dabddfa7e0b1f8337af63b2b8bbbe51b334625a4
mathias brink <mathiasbrink@live.dk>
2018-12-05 10:33:46 +0100

    added log out funktion to customer

---
+++ ViewModelCustomer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using GalaSoft.MvvmLight.Command;
+
+namespace App8
+{
+    class ViewModelCustomer
+    {
+        public ViewModelCustomer()
+        {
+            _logOut = new RelayCommand(LogOut);
+        }
+        private readonly RelayCommand _logOut;
+        public RelayCommand LogOutCommand
+        {
+            get { return _logOut; }
+        }
+
+        public void LogOut()
+        {
+            SessionManager.GetManager().loggedInUser = null;
+            Frame CurrFrame = (Frame)Window.Current.Content;
+            CurrFrame.Navigate(typeof(MainPage));
+        }
+    }
+}
