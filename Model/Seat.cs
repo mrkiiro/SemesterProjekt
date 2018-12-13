5718c87fbb038ff236035059b0321942ed27c546
mrkiiro <kiiro.crafter@gmail.com>
2018-12-13 15:45:26 +0100

    Did some changes

---
+++ Model/Seat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App8.Model
+{
+    class Seat
+    {
+        private bool isTaken;
+
+        public Seat()
+        {
+            isTaken = false;
+        }
+
+        public bool reserve()
+        {
+            if (isTaken)
+            {
+                return false;
+            }
+            else
+            {
+                isTaken = true;
+                return true;
+            }
+        }
+    }
+}
