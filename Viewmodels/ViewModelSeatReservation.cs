9ea90f4e0dd055dc634315d23cff3509db32372d
mathias brink <mathiasbrink@live.dk>
2018-12-13 14:46:42 +0100

    Made a back button

---
+++ Viewmodels/ViewModelSeatReservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using GalaSoft.MvvmLight.Command;
+
+namespace App8
+{
+    class ViewModelSeatReservation
+    {
+        private RelayCommand _changeViewBackToClerkView;
+
+        public ViewModelSeatReservation()
+        {
+            _changeViewBackToClerkView = new RelayCommand(ChangeViewBackToClerkView);
+        }
+        public void ChangeViewBackToClerkView()
+        {
+            Frame CurrFrame = (Frame) Window.Current.Content;
+            CurrFrame.Navigate(typeof(ClerkView));
+        }
+        public RelayCommand ChangeViewBackToClerkViewCommand
+        {
+            get { return _changeViewBackToClerkView; }
+        }
+    }
+}
