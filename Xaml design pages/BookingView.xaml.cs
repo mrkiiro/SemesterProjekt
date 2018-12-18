d1ab8671426905ae8da09146ff5741f559520f45
Oguz Dastan <oguzx@live.dk>
2018-12-18 13:48:47 +0100

    Changes to xaml page

--- Xaml design pages/BookingView.xaml.cs
+++ Xaml design pages/BookingView.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace App8.Xaml_design_pages
+namespace App8
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
