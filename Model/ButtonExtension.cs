b9fea960a1f36c76260ddba3d4a7ce1fc00ca523
Oguz Dastan <oguzx@live.dk>
2018-12-18 13:33:08 +0100

    Creating Button extension class

---
+++ Model/ButtonExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace App8.Model
+{
+    class ButtonExtension : Button
+    {
+        private bool isTaken;
+        public bool IsTaken
+        {
+            get { return isTaken; }
+            set { isTaken = value; }
+        }
+    }
+}
